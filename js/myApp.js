cbca3dd18071def84a1d377e24b01eeb68e9b4e2
J Law <justin.eric.law@gmail.com>
2015-06-16 01:21:13 -0400

    Fix form validation, error message styling, refactor JS

--- js/myApp.js
+++ js/myApp.js
@@ -8,11 +8,13 @@ angular.module('myApp', [])
     var defer = $q.defer();
     $timeout(function(){
       defer.resolve();
-    }, 2000);
+    }, 1200);
     return defer.promise;
   }
 
   $scope.showMessage = '';
+  $scope.submitted = false;
+  $scope.placeholder = "Enter a Tag";
 
   // "Searching Instagram" Text
   function searching() {
@@ -25,57 +27,48 @@ angular.module('myApp', [])
 
   // On valid form submit:
   $scope.submit = function () {
-    $scope.results = null;
 
     if($scope.myForm.$valid) {
       $scope.submitted = true;
       $scope.myForm.$setPristine();
+    
+      // Query made to Instagram API
+      var url = 'https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent';
+      var request = {
+        callback: 'JSON_CALLBACK',
+        client_id: '6d641e362e8d434191c78a2808af92cf',
+      };
+
+      $http({
+        method: 'JSONP',
+        url: url,
+        params: request
+      })
+      .success(function(result) {
+
+        searching().then(function(){
+          console.log('Success');
+          $scope.results = result.data;
+          $scope.showMessage = 'We found ' + $scope.results.length + ' images matching "' + $scope.tag + '"';
+          
+        });
+      })
+      .error(function(result) {
+        searching().then(function(){
+          console.log('Error'); 
+          $scope.showMessage = "Error Searching Instagram";       
+        });
+      });
+    } else if ($scope.myForm.$invalid) {
+      $scope.results = null;
+      $scope.showMessage = '';
     }
-
-    // Query made to Instagram API
-    var url = 'https://api.instagram.com/v1/tags/' + $scope.tag + '/media/recent';
-    var request = {
-      callback: 'JSON_CALLBACK',
-      client_id: '6d641e362e8d434191c78a2808af92cf',
-    };
-
-    $http({
-      method: 'JSONP',
-      url: url,
-      params: request
-    })
-    .success(function(result) {
-
-      searching().then(function(){
-        console.log('Success');
-        $scope.results = result.data;
-        $scope.showMessage = "We found " + $scope.results.length + " images matching " + $scope.tag;
-      });
-    })
-    .error(function(result) {
-      searching().then(function(){
-        console.log('Error'); 
-        $scope.showMessage = "Error Searching Instagram";       
-      });
-    });
 
 
   }; // closes submit call
-
-
-
-  // Show "Results" text:
-  // We found {{results.length}} photos tagged with {{tagname}}
-  
-  // Images should link to Instagram
-  // CLIENT ID: 6d641e362e8d434191c78a2808af92cf
-
-  // If no images found, show "No Images" text
-  // If Query error, show "error" message
-
-
-
 }]); // closes module/controller
 
 
+
+// CLIENT ID: 6d641e362e8d434191c78a2808af92cf
 
